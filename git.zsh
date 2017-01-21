703643c1461eb62080f5fbf440a7e399e492418c
Petros Papadopoulos <petros@ams-ix.net>
2017-01-21 15:06:40 +0100

    Refactored git aliases to use color, added some git functions, added aliases and their descriptions to README

--- git.zsh
+++ git.zsh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
 ###
+# Log with style
+_git_log_medium_format='%C(bold)Commit:%C(reset) %C(green)%H%C(red)%d%n%C(bold)Author:%C(reset) %C(cyan)%an <%ae>%n%C(bold)Date:%C(reset)   %C(blue)%ai (%ar)%C(reset)%n%+B'
+_git_log_oneline_format='%C(green)%h%C(reset) %s%C(red)%d%C(reset)%n'
+_git_log_fullgraph_format='%C(green)%h%C(reset) %<|(50,trunc)%s %C(bold blue)<%an>%C(reset) %C(yellow)(%cd)%C(reset)%C(auto)%d%C(reset)%n'
+_git_log_brief_format='%C(green)%h%C(reset) %s%n%C(blue)(%ar by %an)%C(red)%d%C(reset)%n'
 # Aliases
 ###
 alias g='git'
@@ -30,6 +35,9 @@ alias gcP='git cherry-pick --no-commit'
 alias gcr='git revert'
 alias gcR='git reset "HEAD^"'
 alias gcs='git show'
+alias gcl='git-commit-lost'
+alias gcS='git commit -S'
+alias gpS='git show --pretty=short --show-signature'
 
 # Conflict (C)
 alias gCl='git status | sed -n "s/^.*both [a-z]*ed: *//p"'
@@ -59,7 +67,7 @@ alias gfr='git pull --rebase'
 alias gg='git grep'
 alias ggi='git grep --ignore-case'
 alias ggl='git grep --files-with-matches'
-alias ggL='git grep --files-without-matches'
+alias ggL='git grep --files-without-match'
 alias ggv='git grep --invert-match'
 alias ggw='git grep --word-regexp'
 
@@ -75,12 +83,13 @@ alias gix='git rm -r --cached'
 alias giX='git rm -rf --cached'
 
 # Log (l)
-alias gl='git log --topo-order --decorate --graph --oneline"'
-alias gls='git log --topo-order --stat"'
-alias gld='git log --topo-order --stat --patch --full-diff"'
-alias glo='git log --topo-order"'
-alias glg='git log --topo-order --all --graph"'
-alias glb='git log --topo-order"'
+alias gl='git log --topo-order --pretty=format:"${_git_log_medium_format}"'
+alias gls='git log --topo-order --stat --pretty=format:"${_git_log_medium_format}"'
+alias gld='git log --topo-order --stat --patch --full-diff --pretty=format:"${_git_log_medium_format}"'
+alias glo='git log --topo-order --pretty=format:"${_git_log_oneline_format}"'
+alias glg='git log --topo-order --all --graph --pretty=format:"${_git_log_oneline_format}"'
+alias glG='git log --topo-order --all --graph --pretty=format:"${_git_log_fullgraph_format}" --date=relative'
+alias glb='git log --topo-order --pretty=format:"${_git_log_brief_format}"'
 alias glc='git shortlog --summary --numbered'
 
 # Merge (m)
@@ -96,6 +105,8 @@ alias gpf='git push --force'
 alias gpa='git push --all'
 alias gpA='git push --all && git push --tags'
 alias gpt='git push --tags'
+alias gpc='git push --set-upstream origin "$(git-branch-current 2> /dev/null)"'
+alias gpp='git pull origin "$(git-branch-current 2> /dev/null)" && git push origin "$(git-branch-current 2> /dev/null)"'
 
 # Rebase (r)
 alias gr='git rebase'
@@ -113,17 +124,22 @@ alias gRm='git remote rename'
 alias gRu='git remote update'
 alias gRp='git remote prune'
 alias gRs='git remote show'
+alias gRb='git-hub-browse'
 
 # Stash (s)
 alias gs='git stash'
 alias gsa='git stash apply'
 alias gsx='git stash drop'
+alias gsX='git-stash-clear-interactive'
 alias gsl='git stash list'
+alias gsL='git-stash-dropped'
 alias gsd='git stash show --patch --stat'
 alias gsp='git stash pop'
+alias gsr='git-stash-recover'
 alias gss='git stash save --include-untracked'
 alias gsS='git stash save --patch --no-keep-index'
 alias gsw='git stash save --include-untracked --keep-index'
+alias gsu='git stash show -p | git apply -R'
 
 # Submodule (S)
 alias gS='git submodule'
@@ -132,8 +148,10 @@ alias gSf='git submodule foreach'
 alias gSi='git submodule init'
 alias gSI='git submodule update --init --recursive'
 alias gSl='git submodule status'
+alias gSm='git-submodule-move'
 alias gSs='git submodule sync'
 alias gSu='git submodule foreach git pull origin master'
+alias gSx='git-submodule-remove'
 
 # Working Copy (w)
 alias gws='git status --short'
@@ -143,6 +161,6 @@ alias gwD='git diff --no-ext-diff --word-diff'
 alias gwr='git reset --soft'
 alias gwR='git reset --hard'
 alias gwc='git clean -n'
-alias gwC='git clean -f'
+alias gwC='git clean -df'
 alias gwx='git rm -r'
 alias gwX='git rm -rf'
